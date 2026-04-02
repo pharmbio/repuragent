414ad09dc70c897f57117ae028fa1c7a78c25ce0
Dinh Long Huynh <dinhlong240600@gmail.com>
2026-04-02 13:55:14 +0200

    update agent

--- core/supervisor/supervisor.py
+++ core/supervisor/supervisor.py
@@ -36,7 +36,7 @@ def initialize_agents(
     data_llm = init_chat_model('gpt-5.2', model_provider='openai', streaming=True)
     research_llm = init_chat_model('gpt-5-mini', model_provider='openai', streaming=True)
     prediction_llm = init_chat_model('gpt-5-mini', model_provider='openai', streaming=True)
-    report_llm = init_chat_model('gpt-5.2', model_provider='openai', streaming=True)
+    report_llm = init_chat_model('gpt-5-mini', model_provider='openai', streaming=True)
 
     research_agent = build_research_agent(research_llm, pre_model_hook=pre_model_hook)
     data_agent = build_data_agent(data_llm, message_trimmer=build_llm_input_messages)
